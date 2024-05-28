ee47c25e7e49c0b2de25a8fd53f6efc00ece27a4
dhruvmisra <dhruvmisra@live.com>
2024-05-28 20:30:24 +0530

    Add missing screenshot file back and add user-id header to requests

--- client-script/src/config/index.ts
+++ client-script/src/config/index.ts
@@ -1,3 +1,4 @@
 export const BACKEND_URL = import.meta.env.VITE_BACKEND_URL;
-export const ENABLE_MOCKING = import.meta.env.MODE === "development";
+export const ENABLE_MOCKING = import.meta.env.MODE !== "production" && import.meta.env.VITE_ENABLE_MOCKING === "true";
+export const USER_ID = import.meta.env.VITE_USER_ID;
 export const HEALTH_CHECK_INTERVAL = 5000;
